533322ed283b51fc9c2e1597705b14c5c43e6815
Louis De Sousa <louis.de-sousa@pi-et-ro.net>
2015-01-14 15:04:12 +0100

    add commands

--- Debian_Live_perso.sh
+++ Debian_Live_perso.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #  
-# Script de création de clé USB ou d'image ISO d'utilisation de CESAR ou 
-# STUDIOBOXAUDIO ou STUDIOBOXVIDEO
+# Script de création de clé USB ou d'image ISO d'utilisation de
+# CESAR ou STUDIOBOXAUDIO ou STUDIOBOXVIDEO
 # Louis-Maurice De Sousa
-# louis.de.sousa@crdp.ac-versailles.fr
+# louis.de-sousa@ac-versailles.fr
 #
 # Version : 0.1
 #
@@ -12,12 +12,13 @@
 #   - dans le cas d'une clé USB, création de la clé
 #
 # Utilisation :
-#   - /bin/bash Debian_Live_perso_moi.sh version arch [device_de_la_clé]
-#           - version peut prendre les valeurs « cesar », « studioboxAudio » 
-#               ou « studioboxVideo » et est obligatoire
-#           - arch peut prendre les valeurs « i386 » ou « amd64 » et est obligatoire
-#           - S'il faut créer la clé dans la foulée, il faut spécifier 
-#                sur quel « device » créer la clé, sdb ou sdc ou...
+#  - /bin/bash Debian_Live_perso_moi.sh version arch [device_de_la_clé]
+#    - version peut prendre les valeurs « cesar », « studioboxAudio » 
+#      ou « studioboxVideo » et est obligatoire
+#    - arch peut prendre les valeurs « i386 » ou « amd64 »
+#      et est obligatoire
+#    - S'il faut créer la clé dans la foulée, il faut spécifier 
+#      sur quel « device » créer la clé, sdb ou sdc ou...
 # Améliorations prévues :
 #
 # Dernière modification : 	
@@ -25,7 +26,7 @@
 #
 # Ce script sert à configurer ses paramètres locaux.
 # Il est sous forme d'exemple, et n'est pas à modifier, ni à remonter 
-# sur le Subversion.
+# sur le Git.
 # En faire une copie qui *elle* sera modifiée.
 #
 ####################
@@ -37,24 +38,27 @@ CLE=$3
 ####################
 # À modifier en fonction de l'environnement de travail
 ####################
-MONHOME="/home/lmds"
-# Emplacement du trunk du Subversion « dev_peda »
-REP_DEPOT_PEDA="$MONHOME/Documents/Depot_Subversion/dev_peda/trunk"
+MONHOME="/home/user/Documents"
+# Emplacement du dépôt git de studiobox
+REP_DEPOT_PEDA="$MONHOME/Dépôt_git/studiobox"
 # Emplacement de trunk du Subversion « formation »
-REP_DEPOT_FORMATION="$MONHOME/Documents/Depot_Subversion/Formation/trunk"
-REP1="$MONHOME/Documents/Applis/Debian_live"    # Répertoire où se trouve 
-                                                #   l'arborescence de Debian_live
-REP2="$MONHOME/Documents/Applis/Debian_live_img"    # Répertoire où sauvegarder
-                                                    #   les images obtenues
+REP_DEPOT_FORMATION="$MONHOME/Dépôt_Subversion/Formation/trunk"
+# Répertoire de travail
+REP_WORK="$MONHOME/StudioBoxAudio"
+# Répertoire où se trouve  l'arborescence de DebianLive
+REP_LIVE="$REP_WORK/DebianLive"
+# Répertoire où sauvegarder les images obtenues
+REP_IMG="$REP_WORK/DebianImg"    
 # Répertoires du dépôt Subversion
-REP4="$REP_DEPOT_PEDA/Scripts/Cles_DebianLive"
-REP5="$REP_DEPOT_FORMATION/Education_aux_media/WebRadio/Documentation/Diaporamas"
-DIST="wheezy"
-MIROIRDISTANT="http://ftp.fr.debian.org"    # Mirroir à mettre dans le système
-                                            #   de la clé
-MIROIRLOCAL="http://altair/depot/mirror/ftp.fr.debian.org"           # Mirroir à utiliser pour
-                                            #   construire la clé
+#REP4="$REP_DEPOT_PEDA/Scripts/Cles_DebianLive"
+# Répertoire de la documentation sur le Subversion
+REP_DOC="$REP_DEPOT_FORMATION/Education_aux_media/WebRadio/Documentation/Diaporamas"
+#DIST="wheezy"
+# Mirroir à mettre dans le système de la clé
+MIROIRDISTANT="http://ftp.fr.debian.org"
+# Mirroir à utiliser pour construire la clé
+MIROIRLOCAL="http://altair/depot/mirror/ftp.fr.debian.org"
 
 /bin/bash Debian_Live.sh $MONHOME $REP_DEPOT_PEDA $REP_DEPOT_FORMATION \
-    $REP1 $REP2 $REP4 $REP5 $DIST $MIROIRDISTANT $MIROIRLOCAL \
-    $VERSION $ARCH $CLE 
+	  $REP_LIVE $REP_IMG $REP_DOC $MIROIRDISTANT \
+	  $MIROIRLOCAL $VERSION $ARCH $CLE 
--- README.md
+++ README.md
@@ -3,11 +3,13 @@ Studiobox
 
 Build a live USB key for webradio usage.
 
-working_dir="/home/user"
 1 create a working directory (StudioBoxAudio)
 
 ```sh
-mkdir $working_dir/StudioBoxAudio
+home_dir="/home/user"
+git_studiobox_dir="/home/user/git_repo/studiobox"
+mkdir $home_dir/StudioBoxAudio
+working_dir="$home_dir/StudioBoxAudio"
 ```
 
 2 in this directory create three directories
@@ -16,14 +18,27 @@ mkdir $working_dir/StudioBoxAudio
   one to store the image (DebianImg)
 
 ```sh
-mkdir $working_dir/StudioBoxAudio/DebianLive
-mkdir $working_dir/StudioBoxAudio/DebianChroot
-mkdir $working_dir/StudioBoxAudio/DebianImg
+mkdir $working_dir/DebianLive
+mkdir $working_dir/DebianChroot
+mkdir $working_dir/DebianImg
 ```
 3 create symbolic links for 
   creation_cle.sh
   Debian_Live.sh
   envoie_ftp.sh  
   in StudioBoxAudio
+
+```sh
+ln -s $git_studiobox_dir/creation_cle.sh $working_dir/creation_cle.sh
+ln -s $git_studiobox_dir/Debian_Live.sh $working_dir/dir/Debian_Live.sh
+ln -s $git_studiobox_dir/envoie_ftp.sh $working_dir/envoie_ftp.sh
+```
+
 4 copy Debian_Live_perso.sh as Debian_Live_perso_me.sh in
 the working directory
+
+```sh
+cp $git_studiobox_dir/Debian_Live_perso.sh $working_dir/Debian_Live_perso_me.sh
+```
+
+5 make Debian_Live_perso_me.sh feet to your filesystem
