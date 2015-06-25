c052afc59cab6f3db41c91df062c2d77340b63e8
Geoffrey Gekiere <ggekiere@ac-versailles.fr>
2015-06-25 12:53:55 +0200

    error correction

--- Debian_Live.sh
+++ Debian_Live.sh
@@ -139,13 +139,13 @@ function PreparationStudioBoxAudio
             $REP_LIVE/config/preseed/
     echo "*** Préparation de la version de butt"
     if [ "$ARCH" = "amd64" ]; then
-        rm $REP_LIVE/config/includes.chroot/etc/usr/local/bin/butt-i386
-        mv $REP_LIVE/config/includes.chroot/etc/usr/local/bin/butt-amd64 \
-	$REP_LIVE/config/includes.chroot/etc/usr/local/bin/butt
+        rm $REP_LIVE/config/includes.chroot/usr/local/bin/butt-i386
+        mv $REP_LIVE/config/includes.chroot/usr/local/bin/butt-amd64 \
+	$REP_LIVE/config/includes.chroot/usr/local/bin/butt
     else
-        rm $REP_LIVE/config/includes.chroot/etc/usr/local/bin/butt-amd64
-        mv $REP_LIVE/config/includes.chroot/etc/usr/local/bin/butt-i386 \
-	$REP_LIVE/config/includes.chroot/etc/usr/local/bin/butt
+        rm $REP_LIVE/config/includes.chroot/usr/local/bin/butt-amd64
+        mv $REP_LIVE/config/includes.chroot/usr/local/bin/butt-i386 \
+	$REP_LIVE/config/includes.chroot/usr/local/bin/butt
     fi
 }
 
