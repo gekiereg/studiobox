67ffaf5ebf20023a2812fbd9496b23d62633ff30
Geoffrey Gekiere <ggekiere@ac-versailles.fr>
2016-04-03 21:04:58 +0200

    commit 1 pour studiobox version jessie (merci FX), version 64bits pour l'instant, pour construire l'image, lancer un lb build sans options particulières

---
+++ StudioboxJessie/config/includes.chroot/home/user/.config/openbox/autostart.sh
@@ -0,0 +1,9 @@
+.$GLOBALAUTOSTART
+
+export OOO_FORCE_DESKTOP=gnome
+
+nitrogen --restore &
+tint2 &
+sleep 1 && conky &
+sleep 1 && volumeicon &
+
---
+++ StudioboxJessie/config/includes.chroot/home/user/Scripts/persistence.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# Script de création de la persistance de la clé studiobox
+# Utilisation du script
+#	/bin/bash persistence.sh device_de_la_clé 
+#	* Spécifier sur quel « device » créer la clé, sdb, sdc ou...
+
+# Erreur de syntaxe
+function Erreur
+{
+  echo "Il manque un paramètre"
+  echo ""
+  echo "*** Utilisation du script"
+  echo "	/bin/bash persistence.sh device_de_la_clé"
+  echo ""
+  echo "		* Spécifier sur quel « device » créer la clé, sdb, sdc ou..."
+  exit
+}
+
+
+VERIFPERSIST=$(ls / | grep persistence)
+
+if [ "$VERIFPERSIST" = "persistence.conf" ]; then
+  echo "La persistance existe déjà"
+  echo "Vous n'avez pas besoin de lancer ce script"
+  echo ""
+  echo "Fin du script"
+  exit
+fi
+
+CLE=$1
+PART1="1"
+PART2="2"
+
+if  [ "$CLE" = "" ]
+	then Erreur
+fi
+
+echo "*** Création de la partiton persistante"
+START=`LC_ALL=C sudo parted /dev/$CLE print free | grep Free | grep [MG]B | gawk '{print $1}'`
+END=`LC_ALL=C sudo parted /dev/$CLE print free | grep Free | grep [MG]B | gawk '{print $2}'`
+sudo parted /dev/$CLE mkpart primary ext2 $START $END
+sudo mkfs.ext2 /dev/$CLE$PART2
+echo "*** définition du label « persistence »"
+sudo tune2fs -L persistence /dev/$CLE$PART2
+sudo mount -t ext2 /dev/$CLE$PART2 /mnt
+sleep 5
+echo "*** Effacement du contenu de la partition persistante"
+sudo rm -rf /mnt/*
+PERSIST="persistence.conf"
+echo "*** Copie du fichier « $PERSIST »"
+echo "/ union" >> $PERSIST
+sudo cp $PERSIST /mnt/$PERSIST
+rm $PERSIST
+sudo umount /dev/$CLE$PART2
+echo "Votre clé est désormais persistante"
+echo "Cela signifie que les modifications et réglages que vous effectuerez"
+echo "à partir de maintenant ne seront pas effacés au reboot"
