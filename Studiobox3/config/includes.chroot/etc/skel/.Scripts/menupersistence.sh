73be3ec334cfc0e114177ae5b7b4471586cfc318
Geoffrey Gekiere <ggekiere@ac-versailles.fr>
2016-06-09 15:49:11 +0200

    erreur de conditions sur l'existence des fichiers

--- Studiobox3/config/includes.chroot/etc/skel/.Scripts/menupersistence.sh
+++ Studiobox3/config/includes.chroot/etc/skel/.Scripts/menupersistence.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ -n /persistence.conf ] && [ -n "$HOME/.config/openbox/menupersistence.xml" ]; then
+if [ -e /persistence.conf ] && [ -e "$HOME/.config/openbox/menupersistence.xml" ]; then
 	mv $HOME/.config/openbox/menupersistence.xml $HOME/.config/openbox/menu.xml
 	mv $HOME/.config/openbox/menupersistence2.xml $HOME/.config/openbox/menu2.xml
 	openbox --restart
