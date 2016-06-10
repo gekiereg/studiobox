9fb9bd5475a6bc5d545a007696d14549d729633e
Geoffrey Gekiere <ggekiere@ac-versailles.fr>
2016-06-10 12:53:27 +0200

    ajout message accueil en cas de non persistence / modification autostart.sh en cas de persistence

--- Studiobox3/config/includes.chroot/etc/skel/.Scripts/menupersistence.sh
+++ Studiobox3/config/includes.chroot/etc/skel/.Scripts/menupersistence.sh
@@ -3,5 +3,15 @@
 if [ -e /persistence.conf ] && [ -e "$HOME/.config/openbox/menupersistence.xml" ]; then
 	mv $HOME/.config/openbox/menupersistence.xml $HOME/.config/openbox/menu.xml
 	mv $HOME/.config/openbox/menupersistence2.xml $HOME/.config/openbox/menu2.xml
+	sed -i '/menupersistence/d' .config/openbox/autostart.sh
 	openbox --restart
+else
+	zenity --info --title="Bienvenue sur Studiobox3" --text="Studiobox fonctionne actuellement en mode 'non-persistent'.
+Cela signifie que tous les réglages que vous effectuez et fichiers que vous créez sont effacés à chaque redémarrage.
+Si vous souhaitez utiliser Studiobox en mode 'persistent', et conserver réglages et fichiers au redémarrage,
+utilisez l'outil de création de la persistance, dans le menu principal > 'Administration' > 'Rendre la clé persistante'
+
+Pour mémoire:
+- votre nom d'utilisateur est 'studiobox'
+- Votre mot de passe est 'live'"
 fi
