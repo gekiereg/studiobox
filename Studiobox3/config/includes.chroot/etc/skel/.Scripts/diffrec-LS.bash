62e63214fb2be2229a0b60789d04ad8b49f66d2e
Geoffrey Gekiere <ggekiere@ac-versailles.fr>
2016-06-11 00:38:16 +0200

    ajout config de la qualité de l'enregistrement et de la diffusion par zenity

--- Studiobox3/config/includes.chroot/etc/skel/.Scripts/diffrec-LS.bash
+++ Studiobox3/config/includes.chroot/etc/skel/.Scripts/diffrec-LS.bash
@@ -3,18 +3,50 @@
 # variables générales
 FICHIERCS='.Scripts/config/cs'
 FICHIERPM='.Scripts/config/pm'
+FICHIERQDIFF='.Scripts/config/qdiff'
+FICHIERQREC='.Scripts/config/qrec'
 IP=$(sudo ifconfig  | grep 'inet adr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}')
 
 function configPM {
-#Nommer le point de montage.Tant que la variable est vide attente de la saisie.
-while [ -z ${point[$i]} ]; do
-read -p "Veuillez saisir le nom du point de montage : " point
+# formulaire zenity
+CFPM=$(zenity --forms \
+    --title="Configuration du point de diffusion" \
+    --text="Définition du point de montage et du mot de passe" \
+    --add-entry="Nom du point de diffusion (sous la forme etab-type-ville.mp3 ou etab-type-ville.ogg" \
+    --add-password="Mot de passe" \
+    --add-password="Confirmer le mot de passe" \
+    --separator="|" 2>/dev/null)
+# possibilité d'annuler la config en cliquant sur annuler
+if [ "$?" -eq 1 ]; then
+    exit
+fi
+
+# récupération des valeurs
+point=$(echo $CFPM | cut -d"|" -f1)
+pass=$(echo $CFPM | cut -d"|" -f2)
+pass1=$(echo $CFPM | cut -d"|" -f3)
+while [ "$pass" !=  "$pass1" ] || [ -z "$pass" ]; do
+zenity --info --title="Mot de passe?" --text="Les mots de passe ne coïncident pas. Relance 
+de la configuration du point de diffusion."
+CFPASS=$(zenity --forms \
+    --title="Configuration du mot de passe" \
+    --text="Définition du mot de passe" \
+    --add-password="Mot de passe" \
+    --add-password="Confirmer le mot de passe" \
+    --separator="|" 2>/dev/null)
+pass=$(echo $CFPASS | cut -d"|" -f1)
+pass1=$(echo $CFPASS | cut -d"|" -f2)
 done
 
-#Tant que la variable est vide, j'attends la saisie
-while [ -z ${pass[$i]} ]; do
-read -p "Veuillez saisir le mot de passe: " pass
-done
+##Nommer le point de montage.Tant que la variable est vide attente de la saisie.
+#while [ -z ${point[$i]} ]; do
+#read -p "Veuillez saisir le nom du point de montage : " point
+#done
+#
+##Tant que la variable est vide, j'attends la saisie
+#while [ -z ${pass[$i]} ]; do
+#read -p "Veuillez saisir le mot de passe: " pass
+#done
 
 echo "pm,$point
 pass,$pass" > $FICHIERPM
@@ -23,27 +55,39 @@ Les auditeurs pourront vous écouter à l'adresse suivante: http://webradio.ac-v
 }
 
 function configCS {
-echo "Les cartes sons suivantes sont installées sur votre système :"
-aplay -l
-#Jusqu'a ce que la reponse soit composée par un nombre, j'attends la saisie
-until [[ ${nombre} =~ ^[0-9]+$ ]]; do
-read -p "Quel est l'identifiant de la carte à utiliser ? (0, 1, etc.): " nombre
-done
+FICHIERCS='.Scripts/config/cs'
+FICHIERCS2='.Scripts/config/cs2'
 
-NBRECARTES=$(aplay -l | grep ^"carte $nombre" | wc -l)
+CARTES=$(aplay -l | grep ^carte)
+aplay -l | grep ^carte > $FICHIERCS
+sed -i 's/ /_/g' $FICHIERCS
 
-#si plusieurs cartes ont le même identifiant, on choisit par le numéro de périphérique
-if [ $NBRECARTES -ge 2 ]; then
-	echo "Plusieurs cartes sont identifiées par le chiffre $nombre"
-	echo "Indiquez le numéro de 'périphérique' de la carte à utiliser"
-	echo "(en cas de doute, indiquez '0')"
-	aplay -l | grep ^"carte $nombre"
-	until [[ ${nombre1} =~ ^[0-9]+$ ]]; do
-		read -p "Quel est l'identifiant du périphérique de la carte à utiliser ? (0, 1, etc.): " nombre1
-	done
-else
-	nombre1="0"
-fi
+LISTECARTES=$(cat $FICHIERCS)
+
+CARTE=$(zenity --entry --title="Configuration de la carte son" --text="Veuillez indiquer la carte son à utiliser" $LISTECARTES 2>/dev/null)
+
+nombre=$(echo $CARTE | cut -d":" -f1 | tail -c2)
+nombre1=$(echo $CARTE | cut -d":" -f2 | tail -c2)
+
+##Jusqu'a ce que la reponse soit composée par un nombre, j'attends la saisie
+#until [[ ${nombre} =~ ^[0-9]+$ ]]; do
+#read -p "Quel est l'identifiant de la carte à utiliser ? (0, 1, etc.): " nombre
+#done
+#
+#NBRECARTES=$(aplay -l | grep ^"carte $nombre" | wc -l)
+#
+##si plusieurs cartes ont le même identifiant, on choisit par le numéro de périphérique
+#if [ $NBRECARTES -ge 2 ]; then
+#	echo "Plusieurs cartes sont identifiées par le chiffre $nombre"
+#	echo "Indiquez le numéro de 'périphérique' de la carte à utiliser"
+#	echo "(en cas de doute, indiquez '0')"
+#	aplay -l | grep ^"carte $nombre"
+#	until [[ ${nombre1} =~ ^[0-9]+$ ]]; do
+#		read -p "Quel est l'identifiant du périphérique de la carte à utiliser ? (0, 1, etc.): " nombre1
+#	done
+#else
+#	nombre1="0"
+#fi
 
 echo "$nombre,$nombre1" > $FICHIERCS
 CARTEOK=$(aplay -l | grep ^"carte $nombre" | grep "périphérique $nombre1")
@@ -63,7 +107,7 @@ function verifCS {
 nombre=$(cat $FICHIERCS | cut -d"," -f1)
 nombre1=$(cat $FICHIERCS | cut -d"," -f2)
 DISPO=$(aplay -l | grep ^"carte $nombre" |  grep "périphérique $nombre1")
-if [ -z "$DISPO" ]; then
+if [ -z "$DISPO" ] || [ -z "$nombre" ] ; then
         zenity --info --title="Carte son indisponible" --text="Il semblerait que la carte son configurée soit indisponible.
 Veuillez la reconfigurer (menu 'Outils WebRadio' > 'Configurer la webradio' > 'Configurer la carte son des flux')" 2>/dev/null
 	exit
@@ -76,6 +120,68 @@ if [ "$TYPEPM" != ogg ] && [ "$TYPEPM" != mp3 ]; then
 	zenity --info --text="Votre point de diffusion est mal ou non configuré (il doit être de forme 'type-nom-ville.mp3' ou 'type-nom-ville.ogg'.
 Veuillez le reconfigurer (menu 'Outils WebRadio' > 'Configurer la webradio' > 'Configurer le point de diffusion radio')" 2>/dev/null
 	exit
+fi
+}
+
+function configQREC {
+QREC=$(zenity --scale --min-value=1 --max-value=9 --value=9 --title="Qualité de l'enregistrement" --text="Choisissez la qualité de l'enregistrement. (1 = très faible, 9 = excellente)")
+echo $QREC > $FICHIERQREC
+}
+
+function configQDIFF {
+QDIFF=$(zenity --scale --min-value=1 --max-value=10 --value=5 --title="Qualité de la diffusion" --text="Choisissez la qualité de la diffusion. (1 = très faible, 9 = excellente)")
+echo $QDIFF > $FICHIERQDIFF
+}
+
+function verifQREC {
+CHIFFREVORBISREC=$(cat $FICHIERQREC)
+if [ -n $CHIFFREVORBIS ]; then
+	QUALITEVORBISREC="quality=0.$CHIFFREVORBISREC"
+else
+	QUALITEVORBISREC="quality=0.6"
+fi
+}
+
+function verifQDIFF {
+if [ "$1" = local ] || [ "$1" = airtime ] ; then
+	CHIFFREVORBISDIFF=$(cat $FICHIERQDIFF)
+	if [ -n $CHIFFREVORBISDIFF ]; then
+		QUALITEVORBISDIFF="quality=0.$CHIFFREVORBISDIFF"
+	else
+		QUALITEVORBISDIFF="quality=0.7"
+	fi
+else
+	if [ "$TYPEPM" = ogg ]; then
+		CHIFFREVORBISDIFF=$(cat $FICHIERQDIFF)
+		if [ -n $CHIFFREVORBISDIFF ]; then
+			QUALITEVORBISDIFF="quality=0.$CHIFFREVORBISDIFF"
+		else
+			QUALITEVORBISDIFF="quality=0.5"
+		fi
+	else
+		CHIFFREMP3DIFF=$(cat $FICHIERQDIFF)
+		if [ -n $CHIFFREMP3DIFF ]; then
+			case $CHIFFREMP3DIFF in
+				'1' | '2')
+				QUALITEMP3DIFF='bitrate=64'
+				;;
+				'3' | '4')
+				QUALITEMP3DIFF='bitrate=96'
+				;;
+				'5' | '6')
+				QUALITEMP3DIFF='bitrate=128'
+				;;
+				'7' | '8')
+				QUALITEMP3DIFF='bitrate=256'
+				;;
+				'9')
+				QUALITEMP3DIFF='bitrate=320'
+				;;
+			esac
+		else
+			QUALITEMP3DIFF='bitrate=128'
+		fi	
+	fi
 fi
 }
 
@@ -112,20 +218,17 @@ PASSAIRTIME='webradio'
 USERAIRTIME='source'
 SERVEURACAD='webradio.ac-versailles.fr'
 SERVEURLOCAL='localhost'
-QUALITEVORBISDIFF='quality=0.6'
-QUALITEVORBISREC='quality=0.9'
-QUALITEMP3DIFF='bitrate=128'
 REPREC='Enregistrements'
 FICHIERREC='%Y-%m-%d-%H_%M_%S.ogg'
 # variables zenity
-TEXTEZENREC="Vous retrouverez l'enregistrement dans le répertoire '$REPREC' (fichier .ogg horodaté)"
-TEXTEZENDIFFLOCAL="Le direct se lancera quand vous fermerez cette fenêtre.
+TEXTEZENREC="Vous retrouverez l'enregistrement dans le répertoire '$REPREC' (fichier .ogg horodaté)."
+TEXTEZENDIFFLOCAL="Le direct se lancera quand vous validerez cette boîte de dialogue.
 Pour vous écouter sur le réseau de l'établissement, ouvrir un navigateur web et y indiquer l'url suivante:
 http://$IP:$PORTICECAST/$PMLOCAL"
 TEXTEZENDIFFINT="La diffusion commencera quand vous validerez cette boîte de dialogue.
-Les auditeurs pourront vous écouter à l'adresse suivante
+Les auditeurs pourront vous écouter à l'adresse suivante:
 http://$SERVEURACAD/$PMACAD"
-TEXTEZENAIR="Le flux radio est désormais envoyé vers le serveur Airtime.
+TEXTEZENAIR="Le flux radio sera envoyé vers le serveur Airtime quand vous validerez cette boîte de dialogue.
 Pour diffuser ce flux sur internet, ouvrez Airtime et basculez la source de flux sur 'Source Maître'"
 
 function difflocal {
@@ -179,36 +282,50 @@ if [ "$1" = configureCS ]; then
 	configCS
 elif [ "$1" = configurePM ]; then
 	configPM
+elif [ "$1" = configureQDIFF ]; then
+	configQDIFF
+elif [ "$1" = configureQREC ]; then
+	configQREC
 elif [ "$1" = local ] && [ -z "$2" ]; then
 	verifIP
 	verifCS
+	verifQDIFF $1
 	difflocal
 elif [ "$1" = local ] && [ "$2" = rec ]; then
 	verifIP
 	verifCS
+	verifQDIFF $1
+	verifQREC
 	difflocalrec
 elif [ "$1" = rec ]; then
 	verifCS
+	verifQREC
 	localrec
 elif [ "$1" = internet ] && [ -z "$2" ]; then
 	verifINT
 	verifPM
 	verifCS
+	verifQDIFF $1
 	diffinternet
 elif [ "$1" = internet ] && [ "$2" = rec ]; then
 	verifINT
 	verifPM
 	verifCS
+	verifQDIFF $1
+	verifQREC
 	diffinternetrec
 elif [ "$1" = airtime ] && [ -z "$2" ]; then
 	verifINT
 	verifCS
 	verifAIR
+	verifQDIFF $1
 	diffairtime
 elif [ "$1" = airtime ] && [ "$2" = rec ]; then
 	verifINT
 	verifCS
 	verifAIR
+	verifQDIFF $1
+	verifQREC
 	diffairtimerec
 else
 	echo "Ce script nécessite des arguments pour fonctionner!"
